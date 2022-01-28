f833e35b199019d610a46c2080367ebaa8e2ee61
Tito Candra <tcandra007@gmail.com>
2022-01-28 10:22:33 +0700

    Card list items

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,8 @@
   <v-app dark>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+      :mini-variant="false"
+      :clipped="true"
       fixed
       app
     >
@@ -25,23 +25,11 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
+      :clipped-left="true"
       fixed
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
       <v-btn
         icon
         @click.stop="fixed = !fixed"
@@ -49,38 +37,13 @@
         <v-icon>mdi-minus</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
-      :absolute="!fixed"
       app
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
@@ -92,7 +55,6 @@
 export default {
   data () {
     return {
-      clipped: false,
       drawer: false,
       fixed: false,
       items: [
@@ -102,12 +64,11 @@ export default {
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'mdi-bookmark-box-multiple',
+          title: 'List Anime',
+          to: '/list-anime'
         }
       ],
-      miniVariant: false,
       right: true,
       rightDrawer: false,
       title: 'Anime Station'
---
+++ pages/list-anime.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-row dense>
+    <v-col>
+      <v-layout wrap>
+        <v-flex
+          v-for="(item, i) in items.data"
+          :key="i"
+          md
+          class="pt-5 mx-2"
+        >
+          <v-card
+            :color="item.color"
+            dark
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="text-h5"
+                  v-text="item.attributes.titles.en_jp"
+                />
+
+                <v-card-subtitle v-text="item.type" />
+
+                <v-card-actions>
+                  <v-btn
+                    class="ml-2 mt-5"
+                    outlined
+                    rounded
+                    small
+                  >
+                    Details
+                  </v-btn>
+                </v-card-actions>
+              </div>
+
+              <v-avatar
+                class="ma-3"
+                size="125"
+                tile
+              >
+                <v-img :src="item.attributes.posterImage.small" />
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: []
+    }
+  },
+  async fetch () {
+    this.items = await fetch(
+      'https://kitsu.io/api/edge/anime?filter[text]=one%20piece'
+    ).then(res => res.json())
+  }
+}
+</script>
